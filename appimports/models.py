a75cf7ae99a91f17f2df94ffbfd8334d5f55d58f
kiwavi <dekamotho@gmail.com>
2022-06-01 07:26:47 +0000

    second commit

--- appimports/models.py
+++ appimports/models.py
@@ -7,9 +7,6 @@ class Currency(models.Model):
     Dollar = models.DecimalField(max_digits=9,decimal_places=4)
     Pound = models.DecimalField(max_digits=9,decimal_places=4)
     Euro = models.DecimalField(max_digits=9,decimal_places=4)
-    
-    def __str__(self):
-        return self.name
 
     class Meta:
         verbose_name_plural = "Currencies"
